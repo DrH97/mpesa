4579292cefcf0f24b8111c2a1025c97cc041fe1b
drh <jmnabangi@gmail.com>
2021-08-06 13:19:10 +0300

    Added doc comments for Mpesa Account class and constructor
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/Mpesa/Library/MpesaAccount.php
+++ src/Mpesa/Library/MpesaAccount.php
@@ -3,9 +3,24 @@
 
 namespace DrH\Mpesa\Library;
 
-
+/**
+ * Class MpesaAccount
+ *
+ * @package DrH\Mpesa\Library
+ */
 class MpesaAccount
 {
+    /**
+     * MpesaAccount constructor.
+     *
+     * @param bool $sandbox
+     * @param string $type
+     * @param ?string $shortcode
+     * @param ?string $key
+     * @param ?string $secret
+     * @param ?string $passkey
+     *
+     */
     public function __construct(
         public bool $sandbox = false,
         public string $type = 'PAYBILL',
