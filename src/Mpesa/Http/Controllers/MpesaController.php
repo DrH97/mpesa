b8e170775e3f543724b0b07b04daf16544bfbb06
drh <jmnabangi@gmail.com>
2022-02-03 22:38:08 +0300

    fixes stk callback bug
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/Mpesa/Http/Controllers/MpesaController.php
+++ src/Mpesa/Http/Controllers/MpesaController.php
@@ -64,7 +64,7 @@ class MpesaController extends Controller
      */
     public function stkCallback(Request $request): JsonResponse
     {
-        $this->repository->processStkPushCallback(json_encode($request->body));
+        $this->repository->processStkPushCallback(json_encode($request->Body));
         $resp = [
             'ResultCode' => 0,
             'ResultDesc' => 'STK Callback received successfully',
