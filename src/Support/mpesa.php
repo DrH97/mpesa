8bd7054dac7358f5d0011f4394b9d6fef52f61cf
drh <jmnabangi@gmail.com>
2022-02-12 22:39:31 +0300

    fixes logger
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/Support/mpesa.php
+++ src/Support/mpesa.php
@@ -8,6 +8,7 @@ use DrH\Mpesa\Facades\STK;
 use DrH\Mpesa\Library\MpesaAccount;
 use DrH\Mpesa\Library\Simulate;
 use Illuminate\Support\Facades\Log;
+use Psr\Log\LoggerInterface;
 
 if (!function_exists('mpesa_balance')) {
     function mpesa_balance(): array
@@ -52,28 +53,33 @@ if (!function_exists('mpesa_simulate')) {
     }
 }
 
-$channel = Log::build([
-    'driver' => 'single',
-    'path' => storage_path('logs/mpesa.log'),
-]);
+if (!function_exists('getLogChannel')) {
+    function getLogChannel(): LoggerInterface
+    {
+        return Log::build([
+            'driver' => 'single',
+            'path' => storage_path('logs/mpesa.log'),
+        ]);
+    }
+}
 
 if (!function_exists('mpesaLog')) {
     function mpesaLog(string|array $level, string $message, array $context = []): void
     {
-        Log::log($level, $message, $context);
+        getLogChannel()->log($level, $message, $context);
     }
 }
 
 if (!function_exists('mpesaLogError')) {
     function mpesaLogError(string|array $message, array $context = []): void
     {
-        Log::log('error', $message, $context);
+        getLogChannel()->error($message, $context);
     }
 }
 
 if (!function_exists('mpesaLogInfo')) {
     function mpesaLogInfo(string|array $message, array $context = []): void
     {
-        Log::log('info', $message, $context);
+        getLogChannel()->info($message, $context);
     }
 }
