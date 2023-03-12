0b2ad5928a2f363eecb172cd343108fd46423206
drh <jmnabangi@gmail.com>
2023-03-12 15:43:59 +0300

    fix bug
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/Mpesa/Listeners/C2bPaymentConfirmation.php
+++ src/Mpesa/Listeners/C2bPaymentConfirmation.php
@@ -13,7 +13,8 @@ class C2bPaymentConfirmation
     public function handle(C2bConfirmationEvent $event): void
     {
         $c2b = $event->c2bCallback;
-        //Try to check if this was from STK
+
+        //Try to check if this was from STK, won't work for Till Numbers beware!!!
         $request = MpesaStkRequest::whereReference($c2b->bill_ref_number)->first();
 
         mpesaLogInfo('C2B Listener: ', [$c2b, $event->apiJsonData, $request]);
